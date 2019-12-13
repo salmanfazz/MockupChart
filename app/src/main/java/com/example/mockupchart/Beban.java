128621f133f012d740c96395176ecc252aa99caa
devptsai <devptsai@gmail.com>
2019-12-13 10:38:06 +0700

    Layout Beban

--- app/src/main/java/com/example/mockupchart/Beban.java
+++ app/src/main/java/com/example/mockupchart/Beban.java
@@ -9,15 +9,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
-public class Beban extends Fragment {
+public class Beban extends Fragment implements AdapterView.OnItemSelectedListener{
+    ProgressBar progressBar, progressBar2;
+    Spinner spinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
-        return inflater.inflate(R.layout.activity_home, null);
+        return inflater.inflate(R.layout.activity_beban, null);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        progressBar = (ProgressBar) getView().findViewById(R.id.progressbar);
+        progressBar2 = (ProgressBar) getView().findViewById(R.id.progressbar2);
+        spinner = (Spinner) getView().findViewById(R.id.spinner);
+
+        progressBar.setProgress(20);
+        progressBar2.setProgress(20);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.titles, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
