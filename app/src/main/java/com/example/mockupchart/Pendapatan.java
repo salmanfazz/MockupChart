1f2ff775ac75136411196855a85ea4e07e421660
devptsai <devptsai@gmail.com>
2019-12-13 10:32:12 +0700

    Layout Pendapatan

--- app/src/main/java/com/example/mockupchart/Pendapatan.java
+++ app/src/main/java/com/example/mockupchart/Pendapatan.java
@@ -43,7 +43,6 @@ public class Pendapatan extends Fragment implements AdapterView.OnItemSelectedLi
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
